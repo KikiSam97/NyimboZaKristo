535cb218c03838f7b813e749366d347b638a5c3e
erickogi <erickogi14@gmail.com>
2017-05-21 22:57:34 +0300

    Initial commit

---
+++ app/src/main/java/com/erickogi14gmail/nyimbozakristo/songs.java
@@ -0,0 +1,32 @@
+package com.erickogi14gmail.nyimbozakristo;
+
+/**
+ * Created by kimani kogi on 5/19/2017.
+ */
+
+public class songs {
+
+
+
+  public static String [] titles={
+       "1 : Ninguinira ngai hoti",
+            "2 : Ninguinira ngai hoti",
+            "3 : Ninguinira ngai hoti",
+            "4 : Ninguinira ngai hoti"
+
+    };
+  public static String [] sins={
+          "D/Surface: Surface::setBufferCount(this=0xaf55c200,bufferCount=4)\n" +
+                  "D/GraphicBuffer: register, handle(0xaa14e0c0) (w:720 h:1280 s:720 f:0x1 u:0x000f02)\n" +
+                  "D/OpenGLRenderer: CacheTexture 3 upload: x, y, width height = 0, 0, 1024, 198\n" +
+                  "D/OpenGLRenderer: ProgramCache save to disk, size = 7\n" +
+                  "V/InputMethodManager: onWindowFocus: android.widget.ListView{e658d69 VFED.VC.. .F....ID 32,32-688,419 #7f0d0048 app:id/list_item} softInputMode=272 first=true flags=#81810100\n" +
+                  "V/InputMethodManager: START INPUT: android.widget.ListView{e658d69 VFED.VC.. .F....ID 32,32-688,419 #7f0d0048 app:id/list_item} ic=null tba=android.view.inputmethod.EditorInfo@14e687d controlFlags=#105\n" +
+                  "D/GraphicBuffer: register, handle(0xaa14e910) (w:720 h:1280 s:720 f:0x1 u:0x000f02)\n" +
+                  "D/GraphicBuffer: register, handle(0xaa14e6e0) (w:720 h:1280 s:720 f:0x1 u:0x000f02)",
+          "2 : Ninguinira ngai hoti",
+          "3 : Ninguinira ngai hoti",
+          "4 : Ninguinira ngai hoti"
+
+  };
+}
---
+++ app/src/main/java/com/erickogi14gmail/nyimbozakristo/view.java
@@ -0,0 +1,48 @@
+package com.erickogi14gmail.nyimbozakristo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class view extends AppCompatActivity {
+public TextView a;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        a=(TextView)findViewById(R.id.anyname);
+
+        Intent intent=getIntent();
+
+        int position=  intent.getIntExtra("Songs_ID",0);
+
+        a.setText(songs.sins[position]);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+
+
+
+
+
+
+
+            }
+        });
+    }
+
+}
